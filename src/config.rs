e969105cb2c5915e3a5ede2241fa512db7958cb4
Sinka <xgao@zoyoe.com>
2024-11-10 04:32:02 +0700

    bump version

--- src/config.rs
+++ src/config.rs
@@ -93,7 +93,7 @@ pub fn random_modifier(current_resource: [i64; LOCAL_ATTRIBUTES_SIZE], rand: u64
 
 lazy_static::lazy_static! {
     pub static ref CONFIG: Config = Config {
-        version: "1.0",
+        version: "1.1",
         bounty_cost_base : 2,
         bounty_reward_base: 4,
         entity_attributes: ["Level", "Speed", "Efficiency", "Producitivity"],
