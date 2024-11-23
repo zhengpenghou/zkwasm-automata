60da5d577f9ca84de51ea803581c95b8b8006e12
Glen <glen@zkcross.org>
2024-11-23 16:00:55 +0800

    udpate deposit unit
    Signed-off-by: Glen <glen@zkcross.org>

--- ts/src/deposit.ts
+++ ts/src/deposit.ts
@@ -13,15 +13,24 @@ const proxyContract = new ethers.Contract(process.env.SETTLEMENT_CONTRACT_ADDRES
 
 async function main() {
   proxyContract.on('TopUp', async (l1token: any, address: any, pid_1: bigint, pid_2: bigint, amount: bigint, event: any) => {
-    console.log(`TopUp event received: pid_1=${pid_1.toString()}, pid_2=${pid_2.toString()}, amount=${amount.toString()}`);
+    console.log(`TopUp event received: pid_1=${pid_1.toString()}, pid_2=${pid_2.toString()}, amount=${amount.toString()} wei`) ;
 
-    // Call the deposit function from the Player instance
-    try {
-        console.log(typeof(pid_1), typeof(pid_2), typeof(amount));
-        await player.deposit(pid_1, pid_2, amount);
+
+    //amount in wei to ether
+    let amountInEther = amount / BigInt(10**18);
+    console.log("deposited amount (in ether): ", amountInEther);
+
+    if (amountInEther < 1n) {
+    	console.error("amount must at least 1 Titan(in ether instead of wei)");
+    }else{
+      // Call the deposit function from the Player instance
+      try {
+        console.log(typeof(pid_1), typeof(pid_2), typeof(amount), typeof(amountInEther));
+        await player.deposit(pid_1, pid_2, amountInEther);
         console.log('Deposit successful!');
-    } catch (error) {
+      } catch (error) {
         console.error('Error during deposit:', error);
+      }
     }
   });
 
@@ -29,4 +38,3 @@ async function main() {
 }
 
 main();
-
