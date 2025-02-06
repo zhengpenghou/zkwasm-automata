9cc64783fa732c8ad2bf8d141f812c6380f1ba5b
charlie275691502 <charlie275691502@gmail.com>
2025-02-06 19:56:20 +1100

    integrate with release-v1 mini rollup

--- ts/src/service.ts
+++ ts/src/service.ts
@@ -1,7 +1,27 @@
 import { Service } from "zkwasm-ts-server";
+import {TxWitness} from "zkwasm-ts-server/src/prover";
+import { Express } from "express";
+import {merkleRootToBeHexString} from "zkwasm-ts-server/src/lib.js";
 
-const service = new Service(()=>{return;}, ()=>{return});
-service.initialize();
+const service = new Service(eventCallback, batchedCallback, extra, bootstrap);
+await service.initialize();
+
+
+let currentUncommitMerkleRoot: string = merkleRootToBeHexString(service.merkleRoot);
+
+function extra (app: Express) {
+  
+}
+
 service.serve();
 
+async function bootstrap(merkleRoot: string): Promise<TxWitness[]> {
+  return [];
+}
 
+async function batchedCallback(arg: TxWitness[], preMerkle: string, postMerkle: string) {
+  return;
+}
+
+async function eventCallback(arg: TxWitness, data: BigUint64Array) {
+}
